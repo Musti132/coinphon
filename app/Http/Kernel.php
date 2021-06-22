cc8b00f68fe4cc9fb9acd08e375fc017738ac238
Mustafa al-nashie <44943708+Musti132@users.noreply.github.com>
2021-06-22 10:39:53 +0200

    Cors fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
